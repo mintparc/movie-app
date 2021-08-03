83a272344c0dc53f0bbf893e6f4d5446aebec825
mint <mintparc@icloud.com>
2021-08-03 14:49:43 +0900

    🎨 Styling the Movies
    - Add style file
    - Place Elements

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Movie.css'
 
-function Movie({id, year, title, summary, poster}) {
-  return <h4>{ title }</h4>
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div className="movies_movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <p className="movie_summary">{summary}</p>
+      </div>
+    </div>
+  )
+  
 }
 
 Movie.prototypes = {
