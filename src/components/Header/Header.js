07b56247b3a747375d53d4423b768c512b7619e3
mint <mintparc@icloud.com>
2021-08-24 21:47:20 +0900

    🎨 Update style code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__logo">Movie</h1>
+      <h1 className="header__logo">ㅇㅕㅇ ㅎㅗㅏ</h1>
       <nav className="nav-menu">
       </nav>
     </header>
